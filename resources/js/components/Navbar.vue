54a6e5e771ce1a465de7ae7d9d3ed5d602f7b6e5
Kamvelihle Gwijana <kamvelihle.gwijana@dev.africanideas.co.za>
2024-11-11 11:29:27 +0200

    add table

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -2,7 +2,7 @@
     <div class="navbar-style">
         <a href="tools" class="navbar-link-style">Tools</a>
         <a href="admin" class="navbar-link-style">Admin Center</a>
-        <a href="register-new" class="navbar-link-style">Register User</a>
+        <a href="register-co" class="navbar-link-style">Register Company</a>
     </div>
 </template>
 <script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,16 +11,41 @@
     <body class="min-h-screen flex flex-col text-black">
         <div id="app" class="flex flex-col min-h-screen">
             <!-- Navbar -->
-             <div>
-                <navbar />
-             </div>
-             <div id="search-area">
-                <searchbar />
-             </div>
+             <div><navbar /></div>
+             <div id="search-area"><searchbar /></div>
+
             <!-- Content -->
-            <main class="border border-black rounded-xl flex-grow p-8 pt-20 flex items-center justify-center">
-                <h1>Site content here, Site content here, Site content here.......</h1>
+            <main class="border border-black rounded-xl flex-grow p-8 pt-5 flex items-center justify-center">
+                <table class="table-auto border-collapse border border-slate-500 w-full">
+                    <!-- column headers -->
+                    <thead>
+                        <tr class="bg-gray-200">
+                            <th class="border border-slate-500 p-4 text-left">Company</th>
+                            <th class="border border-slate-500 p-4 text-left">Contact</th>
+                            <th class="border border-slate-500 p-4 text-left">Location</th>
+                        </tr>
+                    </thead>
+                    <!-- table data: rows -->
+                    <tbody>
+                        <tr class="bg-lightblue hover:bg-blue-50">
+                            <td class="border border-slate-500 p-4">BONA!</td>
+                            <td class="border border-slate-500 p-4">+27 123 456 7890</td>
+                            <td class="border border-slate-500 p-4">South Africa</td>
+                        </tr>
+                        <tr class="bg-lightblue hover:bg-blue-50">
+                            <td class="border border-slate-500 p-4">Roland</td>
+                            <td class="border border-slate-500 p-4">+27 123 456 7890</td>
+                            <td class="border border-slate-500 p-4">Switzerland</td>
+                        </tr>
+                        <tr class="bg-lightblue hover:bg-blue-50">
+                            <td class="border border-slate-500 p-4">Alien Worlds</td>
+                            <td class="border border-slate-500 p-4">+27 123 456 7890</td>
+                            <td class="border border-slate-500 p-4">USA</td>
+                        </tr>
+                    </tbody>
+                </table>
             </main>
+            
             <!-- Footer -->
              <div><footer_component /></div>
         </div>
